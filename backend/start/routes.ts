8af2f8edb3b4a63aca136abdcee9c52c5826da81
Achmad Aulia Irsyad <achmadauliairsyad@gmail.com>
2025-12-06 17:47:26 +0800

    penginstalan yaml

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -15,21 +15,7 @@ router.post('/register', '#controllers/authController.register')
 router.post('/login', '#controllers/authController.login')
 router.post('/google-login', '#controllers/authController.googleLogin')
 
-// Bencana routes (public)
-// router.get('/bencana', '#controllers/bencanaController.index')
-// router.get('/bencana/:id', '#controllers/bencanaController.show')
-
-// Bencana routes (protected)
-// router
-//   .group(() => {
-//     router.get('/users', '#controllers/usersController.index')
-//     router.post('/bencana', '#controllers/bencanaController.store')
-//     router.delete('/bencana/:id', '#controllers/bencanaController.destroy')
-//   })
-//   .use(middleware.auth())
-//   .use(middleware.admin())
-
-// Protected routes (need authentication)
+//CRUD admin
 router
   .group(() => {
     router.get('/users', '#controllers/usersController.index')
@@ -40,33 +26,11 @@ router
   .use(middleware.auth())
   .use(middleware.admin())
 
-// Public user routes
-router.get('/users/:id', '#controllers/usersController.show')
-// router.put('/bencana/:id', '#controllers/bencanaController.update')
 
-// GraphQL route (protected)
+  router.get('/users/:id', '#controllers/usersController.show')
+
 router
   .group(() => {
     router.post('/graphql', '#controllers/graphqlController.handle')
   })
   .use(middleware.auth())
-
-// registrasi relawan (harus login dulu)
-// router
-//   .group(() => {
-//     router.post('/bencana/:id/join', '#controllers/regisRelawanController.join')
-//     router.get('/me/registrations', '#controllers/regisRelawanController.myRegistrations')
-//   })
-//   .use(middleware.auth())
-
-// admin melihat semua pendaftar relawan dan mengubah status pendaftaran
-// router
-//   .group(() => {
-//     router.get('/bencana/:id/relawan', '#controllers/regisRelawanController.bencanaVolunteers')
-//     router.put(
-//       '/registrations/:id/status',
-//       '#controllers/regisRelawanController.updateRegistrationStatus'
-//     )
-//   })
-//   .use(middleware.auth())
-//   .use(middleware.admin())
